14c0171bf5f7cbff3a63462d76b832f0a2cd8649
AMZ <ahmedzaki.bm@gmail.com>
2022-06-16 10:38:20 +0300

    - Created a "CommandsManager" class.

---
+++ commands/commands_manager/commands_manager.py
@@ -0,0 +1,17 @@
+from nextcord.ext.commands import Cog, command
+
+from processing.management.logger.logger import Log
+from processing.management.objects.objects_manager import ObjectsManager
+
+
+class CommandsManager(Cog):
+
+    def __init__(self):
+
+        self.__bot = ObjectsManager.get_object_by_name("BotClass")
+
+    @command()
+    async def say(self, ctx, *args):
+        Log.info(f"N$say command has been called!")
+        message = " ".join(args)
+        await ctx.reply(f"The bot says ({message})")
