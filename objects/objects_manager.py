0f7acecbb7cc1965da11c5d47e308303b2560339
AMZ <ahmedzaki.bm@gmail.com>
2022-06-16 10:22:43 +0300

    - Corrected a wrong usage of "Log" class inside "ObjectsManager" class that lead to display the wrong message!

--- objects/objects_manager.py
+++ objects/objects_manager.py
@@ -1,3 +1,5 @@
+from inspect import stack
+
 from processing.management.logger.logger import Log
 
 
@@ -11,7 +13,7 @@ class ObjectsManager:
     @classmethod
     def get_object_by_name(cls, object_name):
 
-        Log.info(f"Trying to get {object_name} instance...")
+        Log.info(f"Trying to get {object_name} instance...", stack()[1])
 
         _object = None
         repr_object_name = None
@@ -21,31 +23,31 @@ class ObjectsManager:
             _object = cls.__objects[object_name]
 
         except KeyError:
-            Log.exception(f"{repr_object_name} doesn't exist!")
+            Log.exception(f"{repr_object_name} doesn't exist!", stack()[1])
             return None
 
         except AttributeError as E:
-            Log.exception(f"_object is not a class! - {E}")
+            Log.exception(f"_object is not a class! - {E}", stack()[1])
             return None
 
         else:
-            Log.debug(f"Found {repr_object_name}")
+            Log.debug(f"Found {repr_object_name}", stack()[1])
             return _object
 
     @classmethod
     def destruct_objects(cls):
         leak_found = False
-        Log.debug("Looking for memory leaks!")
+        Log.debug("Looking for memory leaks!", stack()[1])
         for _object in [*cls.__objects]:
             leak_found = True
-            Log.warning(f"Found a memory leak!, object is {_object}")
+            Log.warning(f"Found a memory leak!, object is {_object}", stack()[1])
             del cls.__objects[_object]
 
         if leak_found:
-            Log.warning("Found some leaks but cleaned them up!")
+            Log.warning("Found some leaks but cleaned them up!", stack()[1])
 
         else:
-            Log.info("No leaks found!")
+            Log.info("No leaks found!", stack()[1])
 
     @classmethod
     def create_object(cls, _object, *args, custom_name=None, **kwargs):
@@ -58,30 +60,30 @@ class ObjectsManager:
             cls.__objects[object_name] = _object(*args, **kwargs)
 
         except AttributeError:
-            Log.exception(f"object '{_object}' is not a class!")
+            Log.exception(f"object '{_object}' is not a class!", stack()[1])
             return None
 
         except Exception as E:
-            Log.exception(f"Some Error occurred while creating object {repr_object_name}!{E}")
+            Log.exception(f"Some Error occurred while creating object {repr_object_name}!{E}", stack()[1])
             return None
 
-        Log.debug(f"Successfully created Object {repr_object_name}!")
+        Log.debug(f"Successfully created Object {repr_object_name}!", stack()[1])
         return cls.__objects[object_name]
 
     @classmethod
     def delete_object(cls, object_name):
 
-        Log.info(f"Trying to delete {object_name} instance...!")
+        Log.info(f"Trying to delete {object_name} instance...!", stack()[1])
         repr_object_name = repr(object_name)
 
         try:
             del cls.__objects[object_name]
 
         except KeyError:
-            Log.exception(f"{repr_object_name} doesn't exist!")
+            Log.exception(f"{repr_object_name} doesn't exist!", stack()[1])
 
         except Exception as E:
-            Log.exception(f"Error happened!{E}")
+            Log.exception(f"Error happened!{E}", stack()[1])
 
         else:
-            Log.debug(f"Successfully deleted {repr_object_name}")
+            Log.debug(f"Successfully deleted {repr_object_name}", stack()[1])
