dac888d586299852faa0e544942062e61fa7e26a
AMZ <ahmedzaki.bm@gmail.com>
2022-07-22 14:53:31 +0300

    - Some fixing + enhancements on `ObjectsManager` class.

--- objects/objects_manager.py
+++ objects/objects_manager.py
@@ -1,6 +1,7 @@
+from sys import exit as _exit
 from inspect import stack
 
-from management.logger.logger import Log
+from logger.logger import Log
 
 
 class ObjectsManager:
@@ -13,77 +14,85 @@ class ObjectsManager:
     @classmethod
     def get_object_by_name(cls, object_name):
 
-        Log.info(f"Trying to get {object_name} instance...", stack()[1])
+        current_stack = stack()[1]
 
-        _object = None
-        repr_object_name = None
+        if not isinstance(object_name, str):
+            Log.error(f"String required, got {type(object_name)} instead!", current_stack)
+            cls.destruct_objects()
+            _exit(-1)
+
+        Log.info(f"Trying to get {object_name} instance...", current_stack)
 
         try:
-            repr_object_name = repr(object_name)
             _object = cls.__objects[object_name]
 
-        except KeyError:
-            Log.exception(f"{repr_object_name} doesn't exist!", stack()[1])
-            return None
-
         except AttributeError as E:
-            Log.exception(f"_object is not a class! - {E}", stack()[1])
-            return None
+            Log.error(f"Error: {E}", current_stack)
+
+        except KeyError:
+            Log.exception(f"'{object_name}' doesn't exist!", current_stack)
+
+        except Exception as E:
+            Log.error(f"Error: {E}")
 
         else:
-            Log.debug(f"Found {repr_object_name}", stack()[1])
+            Log.debug(f"Found '{object_name}'", current_stack)
             return _object
 
     @classmethod
     def destruct_objects(cls):
-        leak_found = False
-        Log.debug("Looking for memory leaks!", stack()[1])
-        for _object in [*cls.__objects]:
-            leak_found = True
-            Log.warning(f"Found a memory leak!, object is {_object}", stack()[1])
+        current_stack = stack()[1]
+        leaks_found = len(cls.__objects) != 0
+
+        Log.debug("Looking for memory leaks!", current_stack)
+        for _object in (*cls.__objects,):
+            Log.warning(f"Found a memory leak!, object is {_object}", current_stack)
             del cls.__objects[_object]
 
-        if leak_found:
-            Log.warning("Found some leaks but cleaned them up!", stack()[1])
+        if leaks_found:
+            Log.warning("Found some leaks but cleaned them up!", current_stack)
 
         else:
-            Log.info("No leaks found!", stack()[1])
+            Log.info("No leaks found!", current_stack)
 
     @classmethod
     def create_object(cls, _object, *args, custom_name=None, **kwargs):
+        current_stack = stack()[1]
 
-        repr_object_name = None
+        if not isinstance(_object, type):
+            Log.error(f"This Static Method Only Supports Classes, got {type(_object)} instead!", current_stack)
+            cls.destruct_objects()
+            _exit(-1)
+
+        object_name = custom_name or _object.__name__
+
+        if object_name in cls.__objects:
+            Log.error(f"{object_name} already exist, please use a custom name for it!")
+            cls.destruct_objects()
+            _exit(-1)
 
         try:
-            object_name = custom_name if custom_name else _object.__name__
-            repr_object_name = f"'{object_name}'"
             cls.__objects[object_name] = _object(*args, **kwargs)
 
-        except AttributeError:
-            Log.exception(f"object '{_object}' is not a class!", stack()[1])
-            return None
-
         except Exception as E:
-            Log.exception(f"Some Error occurred while creating object {repr_object_name}!{E}", stack()[1])
-            return None
+            Log.error(f"Error occurred while instantiating object!\n\tError: {E}", current_stack)
 
-        Log.debug(f"Successfully created Object {repr_object_name}!", stack()[1])
-        return cls.__objects[object_name]
+        else:
+            Log.debug(f"Successfully created Object '{object_name}'!", current_stack)
+            return cls.__objects[object_name]
 
     @classmethod
     def delete_object(cls, object_name):
-
-        Log.info(f"Trying to delete {object_name} instance...!", stack()[1])
-        repr_object_name = repr(object_name)
+        current_stack = stack()[1]
 
         try:
             del cls.__objects[object_name]
 
         except KeyError:
-            Log.exception(f"{repr_object_name} doesn't exist!", stack()[1])
+            Log.error(f"'{object_name}' doesn't exist!", current_stack)
 
         except Exception as E:
-            Log.exception(f"Error happened!{E}", stack()[1])
+            Log.error(f"Error: {E}", current_stack)
 
         else:
-            Log.debug(f"Successfully deleted {repr_object_name}", stack()[1])
+            Log.debug(f"Successfully deleted '{object_name}'", current_stack)
